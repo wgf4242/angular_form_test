8cfa907d5582a95a9ac6feb43a235c366d4e4cbb
wgf4242 <wgf4242@gmail.com>
2017-07-21 20:08:43 +0800

    Angular4 Dynamic Form e.g

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component }       from '@angular/core';
+
+import { QuestionService } from './question.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  template: `
+    <div>
+      <h2>Job Application for Heroes</h2>
+      <dynamic-form [questions]="questions"></dynamic-form>
+    </div>
+  `,
+  providers:  [QuestionService]
 })
 export class AppComponent {
-  title = 'app';
+  questions: any[];
+
+  constructor(service: QuestionService) {
+    this.questions = service.getQuestions();
+  }
 }
