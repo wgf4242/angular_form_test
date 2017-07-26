d4dc838bcb22a3b81ac6f33bc69de5fb5749cc68
wgf4242 <wgf4242@gmail.com>
2017-07-26 21:25:47 +0800

    Convert dynamic-form to a module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,9 @@
-import { Component }       from '@angular/core';
-
-import { QuestionService } from './question.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  providers:  [QuestionService]
+  providers: []
 })
 export class AppComponent {
-  questions: any[];
-
-  constructor(service: QuestionService) {
-    this.questions = service.getQuestions();
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,33 @@
-import { BrowserModule }                from '@angular/platform-browser';
-import { ReactiveFormsModule }          from '@angular/forms';
-import { NgModule }                     from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
-import { RouterModule }   from '@angular/router';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent }                 from './app.component';
-import { DynamicFormComponent }         from './dynamic-form.component';
-import { DynamicFormQuestionComponent } from './dynamic-form-question.component';
+import { AppComponent } from './app.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
+import { DynamicFormModule } from "app/dynamic-form/dynamic-form.module";
+
 const router = [
-      { path: '', component: AppComponent },
-      { path: 'reacitve-form', component: ReactiveFormComponent },
-    ];
+  { path: '', component: AppComponent },
+  { path: 'reacitve-form', component: ReactiveFormComponent },
+];
 
 @NgModule({
   imports: [
-     BrowserModule, ReactiveFormsModule ,
-         RouterModule.forRoot(router)
+    BrowserModule,
+    ReactiveFormsModule,
+    DynamicFormModule,
+    RouterModule.forRoot(router)
   ],
-  declarations: [ AppComponent, DynamicFormComponent, DynamicFormQuestionComponent, ReactiveFormComponent ],
-  bootstrap: [ AppComponent ]
+  
+  declarations: [
+    AppComponent,
+    ReactiveFormComponent
+  ],
+  
+  bootstrap: [AppComponent]
 })
 export class AppModule {
   constructor() {
